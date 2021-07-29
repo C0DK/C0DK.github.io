d7fe3f1143a6ae982cd695db4b7268569bacdf19
Casper Weiss Bang <c@cwb.dk>
2021-07-29 07:25:37 +0200

    Updated stuff

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -6,7 +6,7 @@ import {
 } from "../theme";
 import { FAIcon, } from "./style";
 
-import { faHeart, faBirthdayCake, faSnowman, faBeer, faGhost } from "@fortawesome/free-solid-svg-icons";
+import { faHeart, faBirthdayCake, faSnowman, faBeer, faGhost, faPray} from "@fortawesome/free-solid-svg-icons";
 
 
 export default () => {
@@ -22,7 +22,9 @@ export default () => {
 }
 const getGreetingFromWeekday = (weekday: number) => {
     const greetings: { [Key: string]: ReactElement } = {
+        1: <Greeting>Have a pleasant Monday! <Icon icon={faPray} /></Greeting>, // Friday
         5: <Greeting>Have a nice Friday! <Icon icon={faBeer} /></Greeting>, // Friday
+        7: <Greeting>Remember to relax this Sunday! <Icon icon={faPray} /></Greeting>, // Friday
     }
 
     return greetings[weekday]
@@ -33,10 +35,14 @@ const getGreetingFromDate = (date: Moment) => {
     console.log(index)
     const greetings: { [Key: string]: ReactElement } = {
         "1-1": <NewYearGreeting />,
-        "9-2": <Greeting>Happy My Birthday! <Icon icon={faBirthdayCake} /></Greeting>,
+        "9-2": <Greeting>Happy My Birthday! </Greeting>,
         "14-2": <Greeting>Happy Valentines! <Icon icon={faHeart} /></Greeting>,
         "25-2": <Greeting>Happy Chinese New Year!</Greeting>,
         "31-11": <Greeting>Happy Halloween!<Icon icon={faGhost} /></Greeting>,
+        "29-11": <Greeting>Happy birthday, my love! <Icon icon={faBirthdayCake} /></Greeting>,
+        "20-09": <Greeting>Happy birthday, my cutie! <Icon icon={faBirthdayCake} /></Greeting>,
+        "09-06": <Greeting>Happy birthday, dad! <Icon icon={faBirthdayCake} /></Greeting>,
+        "21-12": <Greeting>Happy birthday, mom! <Icon icon={faBirthdayCake} /></Greeting>,
         "1-12": <ChristmasGreeting />,
         "24-12": <ChristmasGreeting />,
         "25-12": <ChristmasGreeting />,
--- src/components/TagLine.tsx
+++ src/components/TagLine.tsx
@@ -4,9 +4,9 @@ import { Tag, TagRow } from "./style";
 export default () => (
   <TagRow>
     <Tag>Software Engineer</Tag>
-    <Tag>Freelancer</Tag>
+    <Tag>Married (soon!)</Tag>
     <Tag>Dad</Tag>
     <Tag>Open-Source contributor</Tag>
-    <Tag>Nerd</Tag>
+    <Tag>Friend</Tag>
   </TagRow>
 );
