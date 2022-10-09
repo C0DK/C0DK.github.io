76eebaf2265e0b0abef86afe3077303ae30b0f8a
Casper Weiss Bang <c@cwb.dk>
2022-10-09 19:14:05 +0200

    More tags

--- src/components/TagLine.tsx
+++ src/components/TagLine.tsx
@@ -4,10 +4,15 @@ import { Tag, TagRow } from "./style";
 const TagLine = () => (
   <TagRow>
     <Tag>Software Engineer</Tag>
+    <Tag>m.sc. (comp. sci.)</Tag>
     <Tag>Married (soon!)</Tag>
     <Tag>Dad</Tag>
     <Tag>Open-Source contributor</Tag>
     <Tag>Friend</Tag>
+    <Tag>cargo bike riding hippy</Tag>
+    <Tag>sticker collector</Tag>
+    <Tag>Dane</Tag>
+    <Tag>(Lousy) home renovator</Tag>
   </TagRow>
 );
 
